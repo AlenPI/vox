c50cbecddaa43ed4bc2410cc1c6e08178423b00c
Ariel Contini <53163257+dmtxt@users.noreply.github.com>
2021-06-29 20:59:00 -0300

    INSTALACION DE LA PRIMERA VERSION DEL REPRODUCTOR WEB VOX
    HELLO SOLUMEDIA...YOU'VE BEEN RAPED BY A PIRATE-CODER

---
+++ solumedia.js
@@ -0,0 +1,12 @@
+    $("#stream3").myCast({
+	    URL: "https://radios.solumedia.com:10886",
+		version: "2",
+		enable_cors:true,
+		cors:"https://html5radioplayer5us.herokuapp.com",
+	    bg: "black",
+		accent: "red",
+		blur: false,
+		autoplay:true,
+		volume:1,
+		vertical_layout: true
+    });
